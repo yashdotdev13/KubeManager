209abf63d556a98235110bf932a264fa2b8a3a9f
98001yash <yashchauhan.gaya@gmail.com>
2026-08-01 02:15:54 +0530

    add: discovery-server configured

--- services/api-gateway/src/main/java/com/kubemanager/api_gateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/com/kubemanager/api_gateway/ApiGatewayApplication.java
@@ -4,11 +4,13 @@ import com.kubemanager.api_gateway.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		CorsProperties.class
 })
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
