00eda5255a369670360cdc73f7722f9c23b85f18
98001yash <yashchauhan.gaya@gmail.com>
2026-08-01 22:14:51 +0530

    add: user and role entity and repository

---
+++ services/auth-service/src/main/java/com/kubemanager/auth_service/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.kubemanager.auth_service.repository;
+
+import com.kubemanager.auth_service.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
