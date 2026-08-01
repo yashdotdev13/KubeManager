5c055109cbd0a7ff57b7ac0ea62bb572ca07a56f
98001yash <yashchauhan.gaya@gmail.com>
2026-08-02 00:09:48 +0530

    add: userServiceImpl

---
+++ services/auth-service/src/main/java/com/kubemanager/auth_service/service/Impl/PasswordServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kubemanager.auth_service.service.Impl;
+
+
+import com.kubemanager.auth_service.service.PasswordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordServiceImpl implements PasswordService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String encode(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
--- shared/src/main/java/com/kubemanager/exception/ResourceNotFoundException.java
+++ shared/src/main/java/com/kubemanager/exception/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package com.kubemanager.exception;
 
 public class ResourceNotFoundException extends BaseException {
 
-    public ResourceNotFoundException(String message) {
+    public ResourceNotFoundException(ErrorCode roleNotFound, String message) {
         super(ErrorCode.NOT_FOUND, message);
     }
 
