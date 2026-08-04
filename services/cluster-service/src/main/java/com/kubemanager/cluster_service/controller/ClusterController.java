cb1d1eaf49586f8152da82ba2bd3dcaeb2fd641d
98001yash <yashchauhan.gaya@gmail.com>
2026-08-05 04:35:26 +0530

    foundation ready for the cluster-service

---
+++ services/cluster-service/src/main/java/com/kubemanager/cluster_service/controller/ClusterController.java
@@ -0,0 +1,84 @@
+package com.kubemanager.cluster_service.controller;
+
+import com.kubemanager.cluster_service.dto.request.CreateClusterRequest;
+import com.kubemanager.cluster_service.dto.request.UpdateClusterRequest;
+import com.kubemanager.cluster_service.dto.response.ClusterResponse;
+import com.kubemanager.cluster_service.dto.response.ClusterSummaryResponse;
+import com.kubemanager.cluster_service.service.ClusterService;
+import com.kubemanager.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/clusters")
+public class ClusterController {
+
+    private final ClusterService clusterService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<ClusterResponse> createCluster(
+            @Valid @RequestBody CreateClusterRequest request
+    ) {
+
+        return ApiResponse.success(
+                "Cluster created successfully.",
+                clusterService.createCluster(request)
+        );
+    }
+
+    @GetMapping
+    public ApiResponse<List<ClusterSummaryResponse>> getMyClusters() {
+
+        return ApiResponse.success(
+                "Clusters fetched successfully.",
+                clusterService.getMyClusters()
+        );
+    }
+
+    @GetMapping("/{clusterId}")
+    public ApiResponse<ClusterResponse> getClusterById(
+            @PathVariable UUID clusterId
+    ) {
+
+        return ApiResponse.success(
+                "Cluster fetched successfully.",
+                clusterService.getClusterById(clusterId)
+        );
+    }
+
+    @PutMapping("/{clusterId}")
+    public ApiResponse<ClusterResponse> updateCluster(
+
+            @PathVariable("clusterId") UUID clusterId,
+
+            @Valid
+            @RequestBody
+            UpdateClusterRequest request
+    ) {
+
+        return ApiResponse.success(
+                "Cluster updated successfully.",
+                clusterService.updateCluster(clusterId, request)
+        );
+    }
+
+    @DeleteMapping("/{clusterId}")
+    public ApiResponse<Void> deleteCluster(
+            @PathVariable("clusterId") UUID clusterId
+    ) {
+
+        clusterService.deleteCluster(clusterId);
+
+        return ApiResponse.success(
+                "Cluster deleted successfully.",
+                null
+        );
+    }
+}
