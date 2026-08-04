ef95dce93b1f62e7cfcabab505b2d884ce0bf13a
98001yash <yashchauhan.gaya@gmail.com>
2026-08-05 02:48:51 +0530

    add: cluster-service

---
+++ services/cluster-service/src/main/java/com/kubemanager/cluster_service/ClusterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kubemanager.cluster_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterServiceApplication.class, args);
+	}
+
+}
