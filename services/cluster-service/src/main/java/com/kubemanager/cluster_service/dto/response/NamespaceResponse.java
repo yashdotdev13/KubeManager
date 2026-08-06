937f4b89acc98ed9f34eee8cc2e3a7b86c6a647d
98001yash <yashchauhan.gaya@gmail.com>
2026-08-07 01:30:04 +0530

    added the required dtos for the namespace

---
+++ services/cluster-service/src/main/java/com/kubemanager/cluster_service/dto/response/NamespaceResponse.java
@@ -0,0 +1,23 @@
+package com.kubemanager.cluster_service.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NamespaceResponse {
+
+
+    private String name;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+}
---
+++ services/cluster-service/src/main/java/com/kubemanager/cluster_service/dto/response/NamespaceSummaryResponse.java
@@ -0,0 +1,19 @@
+package com.kubemanager.cluster_service.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NamespaceSummaryResponse {
+
+
+    private String name;
+
+    private String status;
+}
