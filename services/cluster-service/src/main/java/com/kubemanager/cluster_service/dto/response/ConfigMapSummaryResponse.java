df038147d487712acc2f2ec77376621f8cc1d811
98001yash <yashchauhan.gaya@gmail.com>
2026-08-10 21:33:09 +0530

    added the configMap dtos

---
+++ services/cluster-service/src/main/java/com/kubemanager/cluster_service/dto/response/ConfigMapSummaryResponse.java
@@ -0,0 +1,21 @@
+package com.kubemanager.cluster_service.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfigMapSummaryResponse {
+
+
+    private String name;
+
+    private String namespace;
+
+    private Integer dataEntries;
+}
